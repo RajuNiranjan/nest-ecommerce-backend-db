711d996850cc9319c5fe201a4b3df1e3d5673c71
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-08-07 17:38:07 +0530

    added post product endpoint

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/createProduct.dto';
+import { Products } from './schema/products.schema';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private readonly productService: ProductsService) {}
+
+  @Post('/create_product')
+  createProduct(
+    @Body() createProductDto: CreateProductDto,
+  ): Promise<{ product: Products }> {
+    return this.productService.createProduct(createProductDto);
+  }
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Products } from './schema/products.schema';
+import { Model } from 'mongoose';
+import { CreateProductDto } from './dto/createProduct.dto';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(
+    @InjectModel(Products.name) private readonly productModel: Model<Products>,
+  ) {}
+
+  async createProduct(
+    createProductDto: CreateProductDto,
+  ): Promise<{ product: Products }> {
+    const {
+      name,
+      description,
+      offerPrice,
+      originalPrice,
+      imageUrl,
+      sizes,
+      material,
+    } = createProductDto;
+
+    if (
+      !name ||
+      !description ||
+      !originalPrice ||
+      !offerPrice ||
+      !sizes ||
+      !imageUrl ||
+      !material
+    ) {
+      throw new UnauthorizedException('All fields are required');
+    }
+
+    try {
+      const product = await this.productModel.findOne({ name });
+      if (product) {
+        throw new UnauthorizedException('product already existed');
+      }
+
+      const newProduct = await this.productModel.create(createProductDto);
+
+      return { product: newProduct };
+    } catch (error) {
+      console.log(error);
+      throw new UnauthorizedException(error);
+    }
+  }
+}
