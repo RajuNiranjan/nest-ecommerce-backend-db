85205811c8ba88f360305c4b56f96e417481f260
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-08-13 09:44:53 +0530

    updated cors in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+  app.setGlobalPrefix('/api');
+  const port = process.env.PORT || 5000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
